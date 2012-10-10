d92ade670493cb9c97b0deead9dcdb9f449286e0
Denis Kolokol <denis_kolokol@ukr.net>
2012-10-10 17:21:48 +0200

    comment in sql

--- docs/postgres_set_params.sql
+++ docs/postgres_set_params.sql
@@ -1,3 +1,4 @@
-﻿ALTER ROLE postgres IN DATABASE sezam SET client_encoding = 'UTF8';
+/* Do this after complete db settings and before dbsync */
+ALTER ROLE postgres IN DATABASE sezam SET client_encoding = 'UTF8';
 ALTER ROLE postgres IN DATABASE sezam SET default_transaction_isolation = 'read committed';
 ALTER ROLE postgres IN DATABASE sezam SET timezone = 'UTC'
