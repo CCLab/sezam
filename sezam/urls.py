664c3e27303d9a0cd6d0b332782ef28489c1388d
Denis Kolokol <denis_kolokol@ukr.net>
2013-02-28 15:54:41 +0100

    urls update

--- sezam/urls.py
+++ sezam/urls.py
@@ -18,11 +18,19 @@ urlpatterns= patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': settings.MEDIA_ROOT}),
+# WARNING! Temporary!
+urlpatterns += patterns('',
+    url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
 
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': settings.STATIC_ROOT})
-    )
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT})
+
+# if settings.DEBUG:
+#     urlpatterns += patterns('',
+#         url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+#             {'document_root': settings.MEDIA_ROOT}),
+
+#         url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+#             {'document_root': settings.STATIC_ROOT})
+#     )
